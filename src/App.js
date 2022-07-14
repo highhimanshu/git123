245f3eb2848bf06b27e0ce06626f4e9a0fc51e62
highhimanshu <himanshu3596@gmail.com>
2022-07-14 17:09:16 +0530

    fist commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+import Header from "./Component/Header";
+
+function App() {
+  return (
+    <div className="container">
+      <Header />
+    </div>
+  );
+}
+
+export default App;
